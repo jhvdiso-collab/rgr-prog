8aaa53b5844de08e19b6d30623252c29f77f11b7
artemkhaha@mail.ru <artemkhaha@mail.ru>
2026-06-19 15:28:06 +0700

    добавлены заголовки и константы

---
+++ playfair1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+const int MATRIX_SIZE = 16;
+const int BYTE_RANGE = 256;
